af743b5ce31301b948e80aac710245633026d293
ibra-kdbra <git-repo-develop@outlook.com>
2026-02-24 13:30:00 +0300

    refactor(main): update root provider wrapping for state management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -17,8 +17,12 @@ export const store = configureStore({
 });
 setupListeners(store.dispatch);
 
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
