e389acc2aa4a285655dfaf0349699ff04b07e97f
ibra-kdbra <git-repo-develop@outlook.com>
2026-02-24 11:30:00 +0300

    refactor(transaction): migrate transaction controller to use Prisma

--- server/src/presentation/controllers/transaction.controller.js
+++ server/src/presentation/controllers/transaction.controller.js
@@ -1,8 +1,16 @@
-import Transaction from "../../data/models/Transactions.js";
+import prisma from "../../data/prisma.js";
 
 export const getTransactions = async (req, res) => {
     try {
-        const transactions = await Transaction.find().limit(50).sort({ createdAt: -1 });
+        const transactions = await prisma.transaction.findMany({
+            take: 50,
+            orderBy: {
+                createdAt: "desc",
+            },
+            include: {
+                products: true,
+            }
+        });
         res.status(200).json(transactions);
     } catch (error) {
         res.status(404).json({ message: error.message });
