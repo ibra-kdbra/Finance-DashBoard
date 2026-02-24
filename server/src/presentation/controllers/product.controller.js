e528b24d6eb91220bb52f2e7711485c433b3401d
ibra-kdbra <git-repo-develop@outlook.com>
2026-02-24 11:25:00 +0300

    refactor(product): migrate product controller to use Prisma

--- server/src/presentation/controllers/product.controller.js
+++ server/src/presentation/controllers/product.controller.js
@@ -1,8 +1,12 @@
-import Product from "../../data/models/Product.js";
+import prisma from "../../data/prisma.js";
 
 export const getProducts = async (req, res) => {
     try {
-        const products = await Product.find();
+        const products = await prisma.product.findMany({
+            include: {
+                transactions: true,
+            }
+        });
         res.status(200).json(products);
     } catch (error) {
         res.status(404).json({ message: error.message });
